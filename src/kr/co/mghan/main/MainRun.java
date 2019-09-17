0778a44dd69788cbcb6864948e374cbde99b7416
Administrator <Administrator@DESKTOP-1JL2KJE>
2019-09-17 09:35:13 +0900

    MainRun , db 사용하도록 수정

--- src/kr/co/mghan/main/MainRun.java
+++ src/kr/co/mghan/main/MainRun.java
@@ -1,7 +1,10 @@
 package kr.co.mghan.main;
 
-import java.util.Hashtable;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import kr.co.mghan.ex.CodeValueNotFoundException;
 import kr.co.mghan.view.CommonMethod;
@@ -11,63 +14,102 @@ public class MainRun extends CommonMethod
 {
 	public static void main(String[] args) throws CodeValueNotFoundException
 	{
-		Map<String, String> map = new Hashtable<String, String>();
 
-		try {
+		try
+		{
 			Class.forName("oracle.jdbc.OracleDriver");
 		}
-		catch(ClassNotFoundException e) {
+		catch (ClassNotFoundException e)
+		{
 			e.printStackTrace();
 		}
-		//oracle driver등록
-		
-		map.put("admin", "adminpw");
-		map.put("hr_head", "user1");
-		map.put("it_mgr", "user2");
-		map.put("cs_emp", "user3");
-		Menu mn = new Menu();
+		// oracle driver등록
+
+		Connection con = null;
+
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+		String user = "scott";
+		String password = "tiger";
+
+		try
+		{
+			con = DriverManager.getConnection(url, user, password);
+		}
+		catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Statement stmt = null;
+		// ResultSet (DB 결과값 저장)를 선언하여 가져오기
+		ResultSet rs = null;
+		// 집어넣을 리스트 생성
+
 		while (true)
 		{
 			System.out.println("아이디와 비밀번호를 입력해주세요.");
-			System.out.print("아이디: ");
-			String id = input_msg();
+			System.out.print("사원명: ");
+			String ename = input_msg();			
 
-			System.out.print("비밀번호: ");
-			String pw = input_msg();
+			System.out.print("사원번호: ");
+			String empno = input_msg();
+			int i_pw = Integer.parseInt(empno);
+
+			String sql = "SELECT empno, ename, job FROM emp WHERE empno = " + i_pw;
+			try
+			{
+				stmt = con.createStatement();
+				rs = stmt.executeQuery(sql);
+				Menu mn = new Menu();
+				if (rs.next()) // 값이 검색이 되었을 시
+				{
+					if (ename.toUpperCase().equals(rs.getString(2)))
+					{						
+						if (Integer.parseInt(empno) == rs.getInt(1))
+						{
+							System.out.println("로그인 되었습니다.");
+							if (rs.getString(3).equals("PRESIDENT")
+									|| rs.getString(3).equals("ANALYST"))
+							{
+								mn.main_menu(0);
+								break;
+							}
+							else if (rs.getString(3).equals("MANAGER"))
+							{
+								mn.main_menu(1);
+								break;
+							}
+							else if (rs.getString(3).equals("SALESMAN"))
+							{
+								mn.main_menu(2);
+								break;
+							}
+							else if (rs.getString(3).equals("CLERK"))
+							{
+								mn.main_menu(3);
+								break;
+							}
+						}
+						else
+						{
+							System.out.println("비밀번호가 일치하지 않습니다.");
+						}
+					}
+					else
+					{
+						System.out.println("입력하신 아이디가 존재하지 않습니다.");
+					}
+				}
+			}
+			catch (SQLException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 			// Menu 객체를 통해 아까 만든 메뉴 실행 시키기
 
-			if (map.containsKey(id))
-			{
-				if (map.get(id).equals(pw))
-				{
-					System.out.println("로그인 되었습니다.");
-					if (id.equals("admin"))
-					{						
-						mn.main_menu(0);
-						break;
-					}
-					else if(id.equals("hr_head")) {						
-						mn.main_menu(1);
-						break;
-					}
-					else if(id.equals("it_mgr")) {
-						mn.main_menu(2);
-						break;
-					}
-					else if(id.equals("cs_emp")) {
-						mn.main_menu(3);
-						break;
-					}
-				}
-				else
-				{
-					System.out.println("비밀번호가 일치하지 않습니다.");
-				}
-			}
-			else
-			{
-				System.out.println("입력하신 아이디가 존재하지 않습니다.");
-			}
 		}
 
 	}
